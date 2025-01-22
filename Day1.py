8c0b85be7b514260a34f06b44fc8943a6ea3b03a
Alisa Sharma <sharmaalisha2024@gmail.com>
2025-01-22 15:01:05 +0545

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,10 @@
+#Day 1: Hello, World!
+
+#1. Write a program that prints "Hello, World!" to the console
+
+print("Hello, WOrld!")
+
+
+#2. Add single-line and multi-line comments to your code
+
+#This is a single line comment.
